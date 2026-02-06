0f7d5677fa767da72d3af49b541782ed1ee3492c
Pamaljith Ranasinghe <pamaljith@getlayup.com>
2026-02-06 11:10:45 +0530

    login mechanism created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email'
+        ]);
 
+        $user = User::firstOrCreate([
+            'email' => $request->email
+        ]);
+
+        $token = $user->createToken('api')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
 }
