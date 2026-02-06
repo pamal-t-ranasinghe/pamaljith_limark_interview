09e8faeb3903bd77e7f9066393c39841b426259a
Pamaljith Ranasinghe <pamaljith@getlayup.com>
2026-02-06 11:29:04 +0530

    listing companies

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -23,5 +23,32 @@ class ContactController extends Controller
 
         return response()->json($contact, 201);
     }
+
+    public function index(Request $request, Company $company)
+    {
+        abort_if($company->user_id !== $request->user()->id, 403);
+
+        $query = $company->contacts();
+
+        if ($request->filled('title')) {
+            $query->where('title', $request->title);
+        }
+
+        if ($request->filled('seniority')) {
+            $query->where('seniority', $request->seniority);
+        }
+
+        if ($request->filled('q')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('first_name', 'like', "%{$request->q}%")
+                    ->orWhere('last_name', 'like', "%{$request->q}%")
+                    ->orWhere('email', 'like', "%{$request->q}%");
+            });
+        }
+
+        return $query->paginate(
+            $request->get('per_page', 10)
+        );
+    }
 }
 
